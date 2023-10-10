6a445a19f6c8a879700622a651ae3ff80f4bb70b
Jose luis barbosa cepeda <barbosa02058@gmail.com>
2023-10-10 12:55:29 -0500

    add backend enviroment url

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000',
     withCredentials: true
 })
